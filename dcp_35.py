1cee74bcde283fdf5a92aaa8690c12b7b521c028
Tanuj Dave <tanuj.dave@printi.com.br>
2019-06-06 12:01:03 +0530

    new file

---
+++ dcp_35.py
@@ -0,0 +1,21 @@
+S = list(['B','B','G','B','R','G', 'R','B','R','G','R', 'B', 'B', 'B', 'R', 'R'])
+v = 0
+
+def moveEl(idx, i, v):
+    v = v + 1
+    if 'R' == i:
+        del S[idx]
+        S.insert(0, i)
+        if 'R' == S[idx] or 'B' == S[idx]:
+            moveEl(idx, S[idx], v)
+
+    if 'B' == i:
+        del S[idx]
+        S.insert(len(S), i)        
+        if 'R' == S[idx] or ('B' == S[idx] and idx != v):
+            moveEl(idx, S[idx], v)
+
+for idx, i in enumerate(S):
+    moveEl(idx, i, v)
+
+print(S)
